91b695c5c7d4be3de21287142dc2dc67a60302f8
Fabiana <fabianete@hotmail.com>
2025-05-15 19:16:52 +0200

    update

--- Exercicio 01/EX15.js
+++ Exercicio 01/EX15.js
@@ -6,8 +6,9 @@ let num2 = 1;
 
 // Imprime o primeiro número da sequência
  console.log (num1);
+ console.log (num2);
 
-for(let i = 0; i < 10; i++ ){
+for(let i = 3; i <= 10; i++ ){
 
     let result = num1 + num2;     
     console.log(result); // Imprime o próximo número da sequência
